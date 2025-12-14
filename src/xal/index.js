3b59e03b85ca78ff61f24364c5444afa49a61e77
ZC <83486674+zc360@users.noreply.github.com>
2025-12-14 12:36:04 +0800

    Update index.js

--- src/xal/index.js
+++ src/xal/index.js
@@ -1,18 +1,21 @@
-import {NativeModules} from 'react-native';
+import { NativeModules } from 'react-native';
 import axios from 'axios';
+
 import UserToken from '../tokens/usertoken';
 import SisuToken from '../tokens/sisutoken';
 import DeviceToken from '../tokens/devicetoken';
 import XstsToken from '../tokens/xststoken';
 import MsalToken from '../tokens/msaltoken';
 import StreamingToken from '../tokens/streamingtoken';
-import {getSettings} from '../store/settingStore';
-import {debugFactory} from '../utils/debug';
+
+import { getSettings } from '../store/settingStore';
+import { debugFactory } from '../utils/debug';
+
 import 'react-native-url-polyfill/auto';
 
 const log = debugFactory('xal/index.js');
 
-const {XalManager} = NativeModules;
+const { XalManager } = NativeModules;
 
 export default class Xal {
   keys;
@@ -24,8 +27,8 @@ export default class Xal {
   _xcloudToken;
 
   _app = {
-    AppId: '000000004c20a908', //'000000004c12ae6f', // 0000000048183522 = working, but minecraft --<<< 000000004c12ae6f works, xbox app
-    TitleId: '328178078', //'328178078', // 1016898439 = working
+    AppId: '000000004c20a908',
+    TitleId: '328178078',
     RedirectUri: 'ms-xal-000000004c20a908://auth',
   };
 
@@ -33,24 +36,22 @@ export default class Xal {
     XalManager.init && XalManager.init();
   }
 
+  // 递归获取设备 token（遇 400 自动重试）
   getDeviceTokenHack() {
     return new Promise((resolve, reject) => {
       this.getDeviceToken()
-        .then(deviceToken => {
-          console.log('getDeviceToken success:', deviceToken);
-          resolve(deviceToken);
-        })
+        .then(resolve)
         .catch(error => {
           if (error.statuscode === 400) {
-            console.log('device token get error, retry...');
+            log.info('getDeviceToken error, retrying...');
             return this.getDeviceTokenHack().then(resolve).catch(reject);
-          } else {
-            reject(error);
           }
+          reject(error);
         });
     });
   }
 
+  // 获取设备 Token
   getDeviceToken() {
     return new Promise((resolve, reject) => {
       const payload = {
@@ -80,24 +81,16 @@ export default class Xal {
         body,
       );
       const headers = {
-        ...{
-          'x-xbl-contract-version': '1',
-          'Cache-Control': 'no-store, must-revalidate, no-cache',
-        },
+        'x-xbl-contract-version': '1',
+        'Cache-Control': 'no-store, must-revalidate, no-cache',
         Signature: signature,
       };
-
-      // log.info('headers:', headers);
-      // log.info('body:', body);
 
       axios
         .post('https://device.auth.xboxlive.com/device/authenticate', body, {
           headers,
         })
-        .then(res => {
-          log.info('getDeviceToken res:', res.data);
-          resolve(new DeviceToken(res.data));
-        })
+        .then(res => resolve(new DeviceToken(res.data)))
         .catch(e => {
           log.error('[getDeviceToken] error:', e);
           reject(e);
@@ -105,21 +98,21 @@ export default class Xal {
     });
   }
 
+  // 获取 Code Challenge
   getCodeChallange() {
     return new Promise(resolve => {
       if (this.codeChallange === undefined) {
         this.codeChallange = XalManager.getCodeChallange();
       }
-
       resolve(this.codeChallange);
     });
   }
 
   getRandomState() {
-    const state = XalManager?.getRandomState() || '';
-    return state;
+    return XalManager?.getRandomState() || '';
   }
 
+  // Sisu 初始认证（获取 code / session）
   doSisuAuthentication(deviceToken, codeChallange, state) {
     return new Promise((resolve, reject) => {
       const payload = {
@@ -145,10 +138,8 @@ export default class Xal {
         body,
       );
       const headers = {
-        ...{
-          'x-xbl-contract-version': '1',
-          'Cache-Control': 'no-store, must-revalidate, no-cache',
-        },
+        'x-xbl-contract-version': '1',
+        'Cache-Control': 'no-store, must-revalidate, no-cache',
         Signature: signature,
       };
 
@@ -157,33 +148,27 @@ export default class Xal {
           headers,
         })
         .then(res => {
-          // log.info('doSisuAuthentication data:', res.data);
-          // log.info('doSisuAuthentication header:', res.headers);
           resolve({
             SessionId: res.headers['x-sessionid'],
             ...res.data,
           });
         })
         .catch(e => {
-          console.log('error:', e);
           log.error('[doSisuAuthentication] error:', e);
+          reject(e);
         });
     });
   }
 
   async getRedirectUri() {
     const deviceToken = await this.getDeviceTokenHack();
-    // log.info('1. [getRedirectUri] deviceToken:', deviceToken);
     const codeChallange = await this.getCodeChallange();
-    // log.info('2. [getRedirectUri] codeChallange:', codeChallange);
     const state = this.getRandomState();
-    // log.info('3. [getRedirectUri] state:', state);
     const sisuAuth = await this.doSisuAuthentication(
       deviceToken,
       codeChallange,
       state,
     );
-    // log.info('4. [getRedirectUri] sisuAuth:', sisuAuth);
     return {
       sisuAuth,
       state,
@@ -195,10 +180,8 @@ export default class Xal {
     const url = new URL(redirectUri);
     const error = url.searchParams.get('error');
     if (error) {
-      // const error_description = url.searchParams.get('error_description');
       return false;
     }
-
     const code = url.searchParams.get('code');
     if (code) {
       const state = url.searchParams.get('state');
@@ -211,31 +194,24 @@ export default class Xal {
         );
       }
     }
-
     return false;
   }
 
   async authenticateUserUsingCode(tokenStore, redirectObject, code, state) {
-    // log.info('[authenticateUserUsingCode] redirectObject:', redirectObject);
-    // log.info('[authenticateUserUsingCode] code:', code);
-    // log.info('[authenticateUserUsingCode] state:', state);
     if (state !== redirectObject.state) {
-      // log.info('Authentication failed: State mismatch')
       return false;
     }
-
     const codeChallange = await this.getCodeChallange();
     const userToken = await this.exchangeCodeForToken(
       code,
       codeChallange.verifier,
     );
-
     tokenStore.setUserToken(userToken);
     tokenStore.save();
-
     return true;
   }
 
+  // Code 换取 User Token
   exchangeCodeForToken(code, codeVerifier) {
     return new Promise((resolve, reject) => {
       const payload = {
@@ -246,25 +222,17 @@ export default class Xal {
         redirect_uri: this._app.RedirectUri,
         scope: 'service::user.auth.xboxlive.com::MBI_SSL',
       };
-
       const body = new URLSearchParams(payload).toString();
       const headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Cache-Control': 'no-store, must-revalidate, no-cache',
       };
 
-      // log.info('[exchangeCodeForToken] headers:', headers);
-      // log.info('[exchangeCodeForToken] body:', body);
-
       axios
         .post('https://login.live.com/oauth20_token.srf', body, {
           headers,
         })
-        .then(res => {
-          // log.info('exchangeCodeForToken data:', res.data);
-          log.info('[exchangeCodeForToken] res:', res.data);
-          resolve(new UserToken(res.data));
-        })
+        .then(res => resolve(new UserToken(res.data)))
         .catch(e => {
           log.error('[exchangeCodeForToken] error:', e);
           reject(e);
@@ -280,7 +248,6 @@ export default class Xal {
         refresh_token: userToken.data.refresh_token,
         scope: 'service::user.auth.xboxlive.com::MBI_SSL',
       };
-
       const body = new URLSearchParams(payload).toString();
       const headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -291,24 +258,17 @@ export default class Xal {
         .post('https://login.live.com/oauth20_token.srf', body, {
           headers,
         })
-        .then(res => {
-          // log.info('exchangeCodeForToken data:', res.data);
-          // log.info('refreshUserToken res:', res.data);
-          resolve(new UserToken(res.data));
-        })
-        .catch(e => {
-          reject(e);
-        });
+        .then(res => resolve(new UserToken(res.data)))
+        .catch(e => reject(e));
     });
   }
 
+  // 修复后的 XSTS 授权（去除废弃字段 DeviceToken / TitleToken）
   doXstsAuthorization(sisuToken, relyingParty) {
     return new Promise((resolve, reject) => {
       const payload = {
         Properties: {
           SandboxId: 'RETAIL',
-          DeviceToken: sisuToken.data.DeviceToken,
-          TitleToken: sisuToken.data.TitleToken.Token,
           UserTokens: [sisuToken.data.UserToken.Token],
         },
         RelyingParty: relyingParty,
@@ -322,31 +282,21 @@ export default class Xal {
         body,
       );
       const headers = {
-        ...{
-          'x-xbl-contract-version': '1',
-          'Cache-Control': 'no-store, must-revalidate, no-cache',
-        },
+        'x-xbl-contract-version': '1',
+        'Cache-Control': 'no-store, must-revalidate, no-cache',
         Signature: signature,
       };
-
-      // log.info('[doXstsAuthorization] headers:', headers)
-      // log.info('[doXstsAuthorization] body:', payload)
 
       axios
         .post('https://xsts.auth.xboxlive.com/xsts/authorize', body, {
           headers,
         })
-        .then(res => {
-          // log.info('doSisuAuthorization res:', res.data);
-          resolve(new XstsToken(res.data));
-        })
-        .catch(e => {
-          reject(e);
-        });
+        .then(res => resolve(new XstsToken(res.data)))
+        .catch(e => reject(e));
     });
   }
 
-  // 获取sisu授权
+  // Sisu 授权（用于刷新等流程）
   doSisuAuthorization(userToken, deviceToken, SessionId) {
     return new Promise((resolve, reject) => {
       const payload = {
@@ -364,7 +314,7 @@ export default class Xal {
           x: XalManager.getKeyX(),
           y: XalManager.getKeyY(),
         },
-        ...(SessionId ? {SessionId: SessionId} : {}),
+        ...(SessionId ? { SessionId } : {}),
       };
 
       const body = JSON.stringify(payload);
@@ -379,46 +329,36 @@ export default class Xal {
         signature: signature,
       };
 
-      // log.info('[doSisuAuthorization] headers:', headers);
-      // log.info('[doSisuAuthorization] body:', body);
-
       axios
         .post('https://sisu.xboxlive.com/authorize', body, {
           headers,
         })
-        .then(res => {
-          // log.info('doSisuAuthorization res:', res.data);
-          resolve(new SisuToken(res.data));
-        })
-        .catch(e => {
-          reject(e);
-        });
+        .then(res => resolve(new SisuToken(res.data)))
+        .catch(e => reject(e));
     });
   }
 
-  // Token retrieval helpers
   async refreshTokens(tokenStore) {
     const curUserToken = tokenStore.getUserToken();
-    log.info('tokenStore:', tokenStore);
-    log.info('[refreshTokens] curUserToken:', curUserToken);
-    if (curUserToken === undefined)
+    if (curUserToken === undefined) {
       throw new Error('User token is missing. Please authenticate first');
+    }
 
     try {
       const userToken = await this.refreshUserToken(curUserToken);
       const deviceToken = await this.getDeviceTokenHack();
-      const sisuToken = await this.doSisuAuthorization(userToken, deviceToken);
+      const sisuToken = await this.doSisuAuthorization(
+        userToken,
+        deviceToken,
+      );
 
       tokenStore.setUserToken(userToken);
       tokenStore.setSisuToken(sisuToken);
       tokenStore.save();
-
-      return {userToken, deviceToken, sisuToken};
+      return { userToken, deviceToken, sisuToken };
     } catch (error) {
-      log.error('[refreshTokens] err:', error);
-      throw new Error(
-        '[refreshTokens] Failed to refresh tokens: ' + JSON.stringify(error),
-      );
+      log.error('[refreshTokens] error:', error);
+      throw error;
     }
   }
 
@@ -445,48 +385,28 @@ export default class Xal {
         .post('https://login.live.com/oauth20_token.srf', body, {
           headers,
         })
-        .then(res => {
-          // log.info('exchangeCodeForToken data:', res.data);
-          log.info(
-            '[exchangeRefreshTokenForXcloudTransferToken] res:',
-            res.data,
-          );
-          resolve(new MsalToken(res.data));
-        })
-        .catch(e => {
-          log.error('[exchangeRefreshTokenForXcloudTransferToken] error:', e);
-          reject(e);
-        });
+        .then(res => resolve(new MsalToken(res.data)))
+        .catch(e => reject(e));
     });
   }
 
   async getMsalToken(tokenStore) {
     const userToken = tokenStore.getUserToken();
-    if (userToken === undefined)
-      throw new Error('User token is missing. Please authenticate first');
-
-    return await this.exchangeRefreshTokenForXcloudTransferToken(userToken);
+    if (!userToken) throw new Error('User token missing');
+    return this.exchangeRefreshTokenForXcloudTransferToken(userToken);
   }
 
   async getWebToken(tokenStore) {
     const sisuToken = tokenStore.getSisuToken();
-    if (sisuToken === undefined)
-      throw new Error('Sisu token is missing. Please authenticate first');
+    if (!sisuToken) throw new Error('Sisu token missing');
 
-    if (
-      this._webToken === undefined ||
-      this._webToken.getSecondsValid() <= 60
-    ) {
-      const token = await this.doXstsAuthorization(
+    if (!this._webToken || this._webToken.getSecondsValid() <= 60) {
+      this._webToken = await this.doXstsAuthorization(
         sisuToken,
         'http://xboxlive.com',
       );
-      this._webToken = token;
-
-      return token;
-    } else {
-      return this._webToken;
     }
+    return this._webToken;
   }
 
   getStreamToken(xstsToken, offering) {
@@ -509,50 +429,36 @@ export default class Xal {
         headers['x-forwarded-for'] = _settings.force_region_ip;
       }
 
-      // log.info(`[getStreamToken]-${offering} headers:`, headers);
-      // log.info(`[getStreamToken]-${offering} body:`, payload);
-
       axios
         .post(
           `https://${offering}.gssv-play-prod.xboxlive.com/v2/login/user`,
           body,
-          {
-            headers,
-          },
+          { headers },
         )
-        .then(res => {
-          // log.info('[getStreamToken] res:', res.data);
-          resolve(new StreamingToken(res.data));
-        })
-        .catch(e => {
-          reject(e);
-        });
+        .then(res => resolve(new StreamingToken(res.data)))
+        .catch(e => reject(e));
     });
   }
 
   async getStreamingToken(tokenStore) {
     const sisuToken = tokenStore.getSisuToken();
-    if (sisuToken === undefined)
-      throw new Error('Sisu token is missing. Please authenticate first');
+    if (!sisuToken) throw new Error('Sisu token missing');
 
     const xstsToken = await this.doXstsAuthorization(
       sisuToken,
       'http://gssv.xboxlive.com/',
     );
 
-    if (
-      this._xhomeToken === undefined ||
-      this._xhomeToken.getSecondsValid() <= 60
-    ) {
+    if (!this._xhomeToken || this._xhomeToken.getSecondsValid() <= 60) {
       this._xhomeToken = await this.getStreamToken(xstsToken, 'xhome');
     }
 
-    if (
-      this._xcloudToken === undefined ||
-      this._xcloudToken.getSecondsValid() <= 60
-    ) {
+    if (!this._xcloudToken || this._xcloudToken.getSecondsValid() <= 60) {
       try {
-        this._xcloudToken = await this.getStreamToken(xstsToken, 'xgpuweb');
+        this._xcloudToken = await this.getStreamToken(
+          xstsToken,
+          'xgpuweb',
+        );
       } catch (error) {
         log.error('[getStreamingToken] xgpuweb error:', error);
         try {
@@ -561,11 +467,15 @@ export default class Xal {
             'xgpuwebf2p',
           );
         } catch (e) {
-          log.error('[getStreamingToken] xgpuwebf2p error:', error);
+          log.error('[getStreamingToken] fallback error:', e);
+          throw e;
         }
       }
     }
 
-    return {xHomeToken: this._xhomeToken, xCloudToken: this._xcloudToken};
+    return {
+      xHomeToken: this._xhomeToken,
+      xCloudToken: this._xcloudToken,
+    };
   }
 }
